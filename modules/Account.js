5ab47047596517fd66129b25876ca4964fd0c6cc
allentsangdev <allentsang04@gmail.com>
2023-04-09 22:00:48 -0400

    update account module

--- modules/Account.js
+++ modules/Account.js
@@ -9,12 +9,16 @@ async function getAddress() {
 // return the balance of a address in WEI
 async function getBalance(address) {
   const balance = await web3.eth.getBalance(address)
-  return balance
+  const accountBalanceObj = {
+    account: address,
+    balance: balance
+  }
+  return accountBalanceObj
 }   
 	
 
 //const x = getAddress().then(console.log)
-//const x = getBalance('0x03d0cf3f4A832C8E2c224BaA4a049110F39E630F').then(console.log)
+const x = getBalance('0x03d0cf3f4A832C8E2c224BaA4a049110F39E630F').then(console.log)
 
 module.exports = {
 	getAddress,
